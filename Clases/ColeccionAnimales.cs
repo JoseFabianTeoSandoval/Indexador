1cd6dfceaaaf0832e37918836b56f04462a1fc4c
JoseFabianTeoSandoval <jteos1@miumg.edu.gt>
2024-04-26 18:22:48 -0600

    Agregar archivos de proyecto.

---
+++ Clases/ColeccionAnimales.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexador.Clases
+{
+    public class ColeccionAnimales
+    {
+        private Animal[] animales;
+
+        public ColeccionAnimales(int cantidad)
+        {
+            animales = new Animal[cantidad];
+        }
+
+        public Animal this[int indice]
+        {
+            get
+            {
+                if (indice < 0 || indice >= animales.Length)
+                {
+                    throw new IndexOutOfRangeException("Índice fuera de rango.");
+                }
+
+                return animales[indice];
+            }
+            set
+            {
+                if (indice < 0 || indice >= animales.Length)
+                {
+                    throw new IndexOutOfRangeException("Índice fuera de rango.");
+                }
+
+                animales[indice] = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Indexador.Clases;
+
+ColeccionAnimales coleccion = new ColeccionAnimales(3);
+
+coleccion[0] = new Animal("Perro");
+coleccion[1] = new Animal("Gato");
+coleccion[2] = new Animal("Pájaro");
+
+Console.WriteLine("Animales en la colección:");
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(coleccion[i].Nombre);
+}
